d60720eb1e91a3821a71d9fcdf5878ed763349ba
Peter Aarestad <aarestad@gmail.com>
2026-01-16 12:19:54 -0600

    load config file if it exists

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod input;
 mod tetrimino;
 mod ui;
 
+use std::path::PathBuf;
 use crate::config::GameConfig;
 use crate::game::Game;
 
@@ -15,9 +16,9 @@ fn main() -> Result<()> {
     let config = GameConfig::default();
 
     // Example: Load custom config
-    // let config_path = PathBuf::from("tetris_config.json");
-    // let config = GameConfig::load_from_file(&config_path)
-    //     .unwrap_or_else(|_| GameConfig::default());
+    let config_path = PathBuf::from("tetris_config.json");
+    let config = GameConfig::load_from_file(&config_path)
+         .unwrap_or_else(|_| GameConfig::default());
 
     let mut game = Game::new(config);
     game.run()?;
